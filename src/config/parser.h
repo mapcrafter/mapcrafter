cb062ee9ade85d6313aab9772889c5ae15e6351b
Moritz Hilscher <moritz.hilscher@gmail.com>
2013-09-20 16:39:20 +0200

    Added some config entries to the config section classes

--- src/config/parser.h
+++ src/config/parser.h
@@ -24,35 +24,69 @@
 
 #include <string>
 #include <vector>
+#include <set>
+#include <boost/filesystem.hpp>
+
+namespace fs = boost::filesystem;
 
 namespace mapcrafter {
 namespace config2 {
 
 class WorldSection {
 private:
+	fs::path input_dir;
 public:
 	WorldSection() {}
 	~WorldSection() {}
 
 	bool parse(const ConfigSection& section, ValidationMessages& validation);
+
+	fs::path getInputDir() const { return input_dir; }
 };
 
 class MapSection {
 private:
+	std::string name_short, name_long;
+	std::string world;
+
+	fs::path texture_dir;
+	std::set<int> rotations;
+	std::string rendermode;
+	int texture_size;
+
+	bool render_unknown_blocks, render_leaves_transparent, render_biomes;
 public:
-	MapSection() {}
+	MapSection()
+		: texture_size(12), render_unknown_blocks(false), render_leaves_transparent(false),
+		  render_biomes(false) {}
 	~MapSection() {}
 
 	bool parse(const ConfigSection& section, ValidationMessages& validation);
+
+	std::string getShortName() const { return name_short; }
+	std::string getLongName() const { return name_long; }
+	std::string getWorld() const { return world; }
+
+	fs::path getTextureDir() const { return texture_dir; }
+	std::set<int> getRotations() const { return rotations; }
+	std::string getRendermode() const { return rendermode; }
+	int getTextureSize() const { return texture_size; }
+
+	bool renderUnknownBlocks() const { return render_unknown_blocks; }
+	bool renderLeavesTransparent() const { return render_leaves_transparent; }
+	bool renderBiomes() const { return render_biomes; }
 };
 
 class ConfigParser {
 private:
+	fs::path output_dir;
 public:
 	ConfigParser() {}
 	~ConfigParser() {}
 
 	bool parse(const std::string& filename, ValidationMessages& validation);
+
+	fs::path getOutputDir() const { return output_dir; }
 
 	bool hasWorld(const std::string& world) const;
 	const std::map<std::string, WorldSection>& getWorlds() const;
