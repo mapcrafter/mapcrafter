05047de444434374521ef4a5bbb72735051609c1
Moritz Hilscher <moritz.hilscher@gmail.com>
2013-11-01 13:46:31 +0100

    Added GPL header stuff to worker.h/cpp.

--- src/render/worker.h
+++ src/render/worker.h
@@ -1,3 +1,22 @@
+/*
+ * Copyright 2012, 2013 Moritz Hilscher
+ *
+ * This file is part of mapcrafter.
+ *
+ * mapcrafter is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * mapcrafter is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with mapcrafter.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef WORKER_H_
 #define WORKER_H_
 
