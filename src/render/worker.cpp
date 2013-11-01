047c1bb9c4c504badec9e5bd304cda2b34c7fbaf
Moritz Hilscher <moritz.hilscher@gmail.com>
2013-11-01 10:34:40 +0100

    Started to work on a new worker class.

---
+++ src/render/worker.cpp
@@ -0,0 +1,23 @@
+/*
+ * worker.cpp
+ *
+ *  Created on: 01.11.2013
+ *      Author: moritz
+ */
+
+#include "worker.h"
+
+namespace mapcrafter {
+namespace render {
+
+Worker::Worker() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Worker::~Worker() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace render */
+} /* namespace mapcrafter */
---
+++ src/render/worker.h
@@ -0,0 +1,46 @@
+#ifndef WORKER_H_
+#define WORKER_H_
+
+#include "../util.h"
+
+#include "../config/mapcrafter_config.h"
+
+#include "../mc/cache.h"
+
+#include "blockimages.h"
+#include "tile.h"
+#include "render.h"
+
+#include <memory>
+#include <set>
+
+namespace mapcrafter {
+namespace render {
+
+class Worker {
+private:
+	std::shared_ptr<mc::WorldCache> world;
+	std::shared_ptr<TileSet> tileset;
+	std::shared_ptr<BlockImages> blockimages;
+	TileRenderer renderer;
+
+	config2::MapcrafterConfigFile config;
+	config2::MapSection map_config;
+	std::set<Path> tiles, tiles_skip;
+public:
+	Worker();
+	~Worker();
+
+	void setWorld(std::shared_ptr<mc::WorldCache> world,
+			std::shared_ptr<TileSet> tileset,
+			std::shared_ptr<BlockImages> blockimages);
+	void setWork(const config2::MapcrafterConfigFile& config,
+			const config2::MapSection& map_config,
+			const std::set<Path>& tiles, const std::set<Path>& tiles_skip);
+
+	void operator()();
+};
+
+} /* namespace render */
+} /* namespace mapcrafter */
+#endif /* WORKER_H_ */
