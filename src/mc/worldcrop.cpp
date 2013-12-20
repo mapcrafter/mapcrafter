98ac16d9a8a06fb834a0af4456412a1848f88037
Moritz Hilscher <moritz.hilscher@gmail.com>
2013-12-20 19:24:36 +0100

    Small mistake: chunk with=16 -> width from one to another corner is 15.

--- src/mc/worldcrop.cpp
+++ src/mc/worldcrop.cpp
@@ -140,9 +140,9 @@ bool WorldCrop::isChunkContained(const mc::ChunkPos& chunk) const {
 bool WorldCrop::isChunkCompletelyContained(const mc::ChunkPos& chunk) const {
 	mc::BlockPos corner(chunk.x * 16, chunk.z * 16, 0);
 	return isBlockContainedXZ(corner)
-			&& isBlockContainedXZ(corner + mc::BlockPos(16, 0, 0))
-			&& isBlockContainedXZ(corner + mc::BlockPos(0, 16, 0))
-			&& isBlockContainedXZ(corner + mc::BlockPos(16, 16, 0));
+			&& isBlockContainedXZ(corner + mc::BlockPos(15, 0, 0))
+			&& isBlockContainedXZ(corner + mc::BlockPos(0, 15, 0))
+			&& isBlockContainedXZ(corner + mc::BlockPos(15, 15, 0));
 }
 
 bool WorldCrop::isBlockContainedXZ(const mc::BlockPos& block) const {
