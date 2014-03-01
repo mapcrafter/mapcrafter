5e07293fca83249d4113ff10ec49678d9bc087de
Moritz Hilscher <moritz.hilscher@gmail.com>
2014-03-01 11:53:38 +0100

    Started to work on new markers (markers.js file) format.

--- src/data/template/markers.js
+++ src/data/template/markers.js
@@ -1,33 +1,21 @@
 // Here you can add your own markers.
 
 var MARKERS = {
-	// You have to specify a world name for the markers.
-	// This is the last part of the path to the world.
-	// For example: The name for "/this/is/my_world" is just "my_world".
-	"worldname": [
-		// A simple example for a marker:
-		{"pos": [0, 0, 64], "title": "Spawnpoint"},
-		// The position of a marker is always in the form [x, z, y].
-		
-		// You can add your own icon:
-		{"pos": [12, 34, 64], "title": "Marker", "icon": "icons/myicon.png"},
-		// Just copy your icon file into your template directory to use it here.
-		// A size of 24x24 pixel for your icons would be good.
-		// Otherwise you should also specify the size of your icon (for the Leaflet template):
-		{"pos": [12, 34, 64], "title": "Marker", "icon": "icons/myicon.png", "iconsize": [32, 32]},
-		
-		// And/or you can set the text of the marker window:
-		{"pos": [34, 12, 64], "title": "Another Marker", "text": "This is <b>another</b> marker"},
-	],
-	
-	// And you can specify the markers of your other worlds:
-	"world2": [
-		{"pos": [0, 0, 64], "title": "Marker"},
-	],
-	
-	"world2": [
-		{"pos": [0, 0, 64], "title": "Marker"},
-	],
-	
-	// etc.
+	"homes" : {
+		"icon" : "bed.png",
+		"markers" : {
+			"world1.4" : [
+				{"pos" : [42, 73, 64], "title" : "My Home"},
+				{"pos" : [100, 300, 64], "title" : "Steve's home", "text" : "This is Steve's home."},
+			],
+		},
+	},
+	"spawn" : {
+		"icon" : "diamond.png",
+		"markers" : {
+			"world1.4" : [
+				{"pos" : [0, 0, 64], "title" : "Spawn"},
+			],
+		},
+	},
 };
--- src/data/template/static/js/handler/marker.js
+++ src/data/template/static/js/handler/marker.js
@@ -4,34 +4,48 @@ MarkerHandler.prototype = new BaseHandler();
  * Is responsible to show the markers from the markers.js.
  */
 function MarkerHandler(markers) {
-	this.mapMarkers = [];
+	this.layerGroups = {};
 	this.markers = markers;
 }
 
 MarkerHandler.prototype.onMapChange = function(name, rotation) {
-	for(var i = 0; i < this.mapMarkers.length; i++)
-		this.ui.lmap.removeLayer(this.mapMarkers[i]);
-	this.mapMarkers = [];
+	for(var group in this.layerGroups)
+		this.ui.lmap.removeLayer(this.layerGroups[group]);
+	this.layerGroups = {};
 	
 	var world = this.ui.getCurrentMapConfig().worldName;
-	if(!(world in this.markers))
-		return;
-	for(var i = 0; i < this.markers[world].length; i++) {  
-		var location = this.markers[world][i];
-		
-		var pos = location.pos;
-		var marker = new L.Marker(this.ui.mcToLatLng(pos[0], pos[1], pos[2]), {
-			title: location.title,
-		});
-		if(location.icon) {
-			marker.setIcon(new L.Icon({
-				iconUrl: location.icon != "" ? "static/markers/" + location.icon : null,
-				iconSize: (location.icon_size ? location.icon_size : [24, 24]),
-			}));
+	for(var group in this.markers) {
+		if (!(world in this.markers[group]["markers"]))
+			continue;
+		var groupInfo = this.markers[group];
+		var markers = groupInfo["markers"][world];
+		var layerGroup = L.layerGroup();
+		for (var i = 0; i < markers.length; i++) {
+			var poi = markers[i];
+			
+			var pos = poi.pos;
+			var marker = new L.Marker(this.ui.mcToLatLng(pos[0], pos[1], pos[2]), {
+				title: poi.title,
+			});
+			if(groupInfo.icon) {
+				marker.setIcon(new L.Icon({
+					iconUrl: groupInfo.icon != "" ? "static/markers/" + groupInfo.icon : null,
+					iconSize: (groupInfo.icon_size ? groupInfo.icon_size : [24, 24]),
+				}));
+			}
+			marker.bindPopup(poi.text ? poi.text : poi.title);
+			marker.addTo(layerGroup);
 		}
-		marker.bindPopup(location.text ? location.text : location.title);
-		marker.addTo(this.ui.lmap);
-		
-		this.mapMarkers.push(marker);
+		layerGroup.addTo(this.ui.lmap);
+		this.layerGroups[group] = layerGroup;
 	}
+	
+	//this.show("spawn", false);
+	//this.show("spawn", true);
+};
+
+MarkerHandler.prototype.show = function(group, visible) {
+	this.layerGroups[group].eachLayer(function(layer) {
+		layer.setOpacity(visible ? 1 : 0);
+	});
 };
