8cc40b427623fca37babd06e4ba419045dfd71a1
Moritz Hilscher <moritz.hilscher@gmail.com>
2014-03-01 11:31:15 +0100

    Fixed different icon size handling in markers.js (forgot to s/iconsize/icon_size/)

--- src/data/template/static/js/handler/marker.js
+++ src/data/template/static/js/handler/marker.js
@@ -26,7 +26,7 @@ MarkerHandler.prototype.onMapChange = function(name, rotation) {
 		if(location.icon) {
 			marker.setIcon(new L.Icon({
 				iconUrl: location.icon != "" ? "static/markers/" + location.icon : null,
-				iconSize: (location.iconsize ? location.iconsize : [24, 24]),
+				iconSize: (location.icon_size ? location.icon_size : [24, 24]),
 			}));
 		}
 		marker.bindPopup(location.text ? location.text : location.title);
