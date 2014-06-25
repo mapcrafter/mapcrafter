61094cbb09919c4b91c4b68ef4b8d4c8f8ad9fcb
Moritz Hilscher <moritz.hilscher@gmail.com>
2014-06-25 11:20:00 +0200

    Started working on new configuration parser class.

---
+++ src/mapcraftercore/config/configparser.cpp
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2012-2014 Moritz Hilscher
+ *
+ * This file is part of Mapcrafter.
+ *
+ * Mapcrafter is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Mapcrafter is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Mapcrafter.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "configparser.h"
+
+namespace mapcrafter {
+namespace config {
+
+ConfigParser::ConfigParser() {
+}
+
+ConfigParser::~ConfigParser() {
+}
+
+const ValidationMap& ConfigParser::validate() {
+	return validation;
+}
+
+} /* namespace config */
+} /* namespace mapcrafter */
---
+++ src/mapcraftercore/config/configparser.h
@@ -0,0 +1,69 @@
+/*
+ * Copyright 2012-2014 Moritz Hilscher
+ *
+ * This file is part of Mapcrafter.
+ *
+ * Mapcrafter is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Mapcrafter is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Mapcrafter.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef CONFIGPARSER_H_
+#define CONFIGPARSER_H_
+
+#include "validation.h"
+
+#include <vector>
+
+namespace mapcrafter {
+namespace config {
+
+/**
+ * A class to parse and validate arbitrary configuration files.
+ */
+class ConfigParser {
+public:
+	ConfigParser();
+	~ConfigParser();
+
+	/**
+	 * Parses the root section of the configuration with the supplied section type object.
+	 */
+	template<typename T>
+	void parseRootSection(T& section, const INIConfigSection& config_section) {
+		section.parse(config, validation);
+	}
+
+	/**
+	 * Parses all sections with a specific type and puts the parsed section type objects
+	 * into the supplied vector<T>. It also parses the global section ([global:<name>s],
+	 * if it exists) and uses it as default for the sections.
+	 */
+	template<typename T>
+	void parseSections(std::vector<T>& sections, const std::string& name,
+			const INIConfig& config) {
+	}
+
+	/**
+	 * Returns the validation of the parsed sections. Also adds warnings for unknown
+	 * section types.
+	 */
+	const ValidationMap& validate();
+
+private:
+	ValidationMap validation;
+};
+
+} /* namespace config */
+} /* namespace mapcrafter */
+
+#endif /* CONFIGPARSER_H_ */
