38454dc37cc50b0d1fc24be35adb91f5c67b5ea0
Moritz Hilscher <moritz.hilscher@gmail.com>
2016-03-17 19:06:16 +0100

    Improved spawn overlay.

--- src/mapcraftercore/renderer/rendermodes/spawnoverlay.cpp
+++ src/mapcraftercore/renderer/rendermodes/spawnoverlay.cpp
@@ -29,13 +29,36 @@ SpawnOverlay::SpawnOverlay(std::shared_ptr<config::ConfigSection> overlay_config
 	  spawn(rgba(255, 0, 0, 85)), no_spawn(rgba(0, 0, 0, 0)) {
 }
 
+/**
+ * There are some blocks where no mobs can spawn.
+ */
+bool isNegativeSpawnException(const mc::Block& block) {
+	return block.id == 8 || block.id == 9 // water
+		|| block.id == 10 || block.id == 11 // lava
+		;
+}
+
+/**
+ * There are some exceptions where mobs can spawn.
+ */
+bool isPositiveSpawnException(const mc::Block& block) {
+	// mobs can spawn on upside-down slabs (no need to mention the double slabs here)
+	if (block.id == 44 || block.id == 126 || block.id == 181 || block.id == 182) {
+		return block.data & 0x8;
+	}
+	// mobs can spawn on upside-down stairs
+	return block.isStairs() && block.data & 0x4;
+}
+
 RGBAPixel SpawnOverlay::getBlockColor(const mc::BlockPos& pos,
 		const mc::BlockPos& for_pos, uint16_t id, uint16_t data) {
 	// TODO is that interpretation of where mobs can spawn correct?
 	
 	// don't colorize sides of blocks or transparent blocks (where pos == for_pos)
-	if (pos.y - for_pos.y == 0)
+	// except snow!
+	if (id != 78 && pos.y - for_pos.y == 0)
 		return no_spawn;
+
 	mc::Block block = getBlock(pos, mc::GET_ID | mc::GET_DATA | mc::GET_LIGHT);
 	LightingData light = LightingData::estimate(block, images, world, *current_chunk);
 	uint8_t light_level = light.getLightLevel(config->isDay());
@@ -43,7 +66,11 @@ RGBAPixel SpawnOverlay::getBlockColor(const mc::BlockPos& pos,
 		// a mob can spawn on the block below if it is not transparent
 		// and the light level of this block is smaller than 8
 		mc::Block below = getBlock(pos + mc::DIR_BOTTOM, mc::GET_ID | mc::GET_DATA);
-		if (below.id != 0 && !images->isBlockTransparent(below.id, below.data))
+		// there are always some exceptions...
+		if (isNegativeSpawnException(below))
+			return no_spawn;
+		if (isPositiveSpawnException(below)
+				|| (below.id != 0 && !images->isBlockTransparent(below.id, below.data)))
 			return spawn;
 	}
 	return no_spawn;
