9b6ce8c4737379d654c400618575eacdc1a66a42
Moritz Hilscher <moritz.hilscher@gmail.com>
2016-11-15 14:37:51 +0100

    Added check if chunk size is zero (#239).

--- src/mapcraftercore/mc/region.cpp
+++ src/mapcraftercore/mc/region.cpp
@@ -140,16 +140,22 @@ bool RegionFile::read() {
 		if (offset == 0)
 			continue;
 
+		// i = x + z * 32
+		int x = i % 32;
+		int z = (i - x) / 32;
+
 		// get data size and compression type
 		uint32_t size = *(reinterpret_cast<uint32_t*>(&regiondata[offset]));
+		if (size == 0) {
+			LOG(ERROR)  << "Corrupt region '" << filename << "': Size of chunk "
+				<< x << ":" << z << " is zero.";
+			return false;
+		}
 		size = util::bigEndian32(size) - 1;
 		uint8_t compression = regiondata[offset + 4];
 		if (filesize < offset + 5 + size) {
-			// i = x + z * 32
-			int x = i % 32;
-			int z = (i - x) / 32;
 			LOG(ERROR) << "Corrupt region '" << filename << "': Invalid size of chunk "
-					<< x << ":" << z << ".";
+				<< x << ":" << z << ".";
 			return false;
 		}
 
